448bf319b5bc46a7c96ba98fb1948e57030df505
MageLink <mage.link.zhezard@gmail.com>
2022-07-29 17:12:45 -0400

    CompletedActivies work

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,21 +1,21 @@
 let arrayTodoList = [
   {
-    id: 001,
+    id: "001",
     activity: "Ver películas",
     completed: false,
   },
   {
-    id: 002,
+    id: "002",
     activity: "Ver anime",
     completed: false,
   },
   {
-    id: 003,
+    id: "003",
     activity: "Ver series",
     completed: true,
   },
   {
-    id: 004,
+    id: "004",
     activity: "Ver youtube",
     completed: false,
   },
@@ -25,25 +25,38 @@ const totalActivities = (elements) => {
   return elements.length;
 };
 
+const completedActivities = (elements) => {
+  const newArray = elements.filter((e) => {
+    return e.completed;
+  });
+  return newArray.length;
+};
+
 const loadActivities = (elements) => {
   for (let i = 0; i < elements.length; i++) {
-    console.log(elements);
     document.querySelector(".checkbox-container").innerHTML += `
     <div class="checkbox-div">
         <span class="id-activy">${elements[i].id}</span>
         <span class="task">${elements[i].activity}</span>
-        <input type="checkbox" ${elements[i].completed && "checked"} />
+        <input type="checkbox" value=${elements[i].id} ${
+      elements[i].completed && "checked"
+    } onclick="addNewActivity()" />
     </div>
         `;
   }
 };
 
+const addNewActivity = () => {
+  console.log("Esto funciona");
+};
+
 const ignorarEsto = () => {
-  console.log(document.querySelector("#4"));
+  console.log(document.querySelectorAll("input"));
 };
 
 window.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".checkbox-container").innerHTML = "";
   document.querySelector("#total-activities").innerHTML = totalActivities(arrayTodoList);
+  document.querySelector("#completed-activities").innerHTML = completedActivities(arrayTodoList);
   loadActivities(arrayTodoList);
 });
